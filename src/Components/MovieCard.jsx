9250375e052213137fb2b7e13ec032d846f93f71
Anuj Jain <jainanuj744@gmail.com>
2023-07-20 19:38:10 +0530

    movies component complete

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,20 +1,38 @@
 function MovieCard(props) {
-    return (
-        <div>
-            <div className="flex flex-wrap justify-center">
-                <div
-                    className="m-4 bg-cover bg-center h-[12rem] w-[10rem] flex items-end rounded-2xl hover:scale-110 cursor-pointer duration-300"
-                    style={{
-                        backgroundImage: `url(https://image.tmdb.org/t/p/original/${props.poster_path})`,
-                    }}
-                >
-                    <div className="text-white bg-gray-500/50 w-full text-center rounded-2xl ">
-                        {props.name}
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="flex flex-wrap justify-center">
+        <div
+          className="m-4 bg-cover bg-center h-[18rem] w-[14rem] 
+                    flex items-end rounded-2xl hover:scale-110 cursor-pointer duration-300
+                    flex-col justify-between"
+          style={{
+            backgroundImage: `url(https://image.tmdb.org/t/p/original/${props.poster_path})`,
+          }}
+        >
+          {props.watchList.includes(props.id) ? (
+            <div
+              className="m-3 h-10 w-10 bg-gray-900/60 flex items-center justify-center rounded-2xl"
+              onClick={() => props.handleRemoveFromWatchList(props.id)}
+            >
+              &#10060;
             </div>
+          ) : (
+            <div
+              className="m-3 h-10 w-10 bg-gray-900/60 flex items-center justify-center rounded-2xl"
+              onClick={() => props.handleAddToWatchList(props.id)}
+            >
+              &#128525;
+            </div>
+          )}
+
+          <div className="text-white bg-gray-500/50 w-full text-center rounded-2xl ">
+            {props.name}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default MovieCard;
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -4,38 +4,76 @@ import { useEffect, useState } from "react";
 import Pagination from "./Pagination";
 
 function Movies() {
-
   let [movies, setMovies] = useState([]);
 
-  let [pageNo,setPageNo] = useState(1);
+  let [pageNo, setPageNo] = useState(1);
 
-    let handleClickPrev = () => {
-        if(pageNo>1)
-        setPageNo(pageNo-1);
-    }
+  let [watchList, setWatchList] = useState([]);
 
-    let handleClickNext = () => {
-        setPageNo(pageNo+1);
-    }
+  let handleClickPrev = () => {
+    if (pageNo > 1) setPageNo(pageNo - 1);
+  };
 
+  let handleClickNext = () => {
+    setPageNo(pageNo + 1);
+  };
+
+  let handleAddToWatchList = (movieId) => {
+    // watchList.push(movieId); it will not work since the reference is same
+    let list = [...watchList, movieId];
+    localStorage.setItem("IMDB", JSON.stringify(list));
+    setWatchList(list);
+  };
+
+  let handleRemoveFromWatchList = (movieId) => {
+    // watchList.push(movieId); it will not work since the reference is same
+    let filterdList = watchList.filter((id) => {
+      return id !== movieId;
+    });
+    localStorage.setItem("IMDB", JSON.stringify(filterdList));
+    setWatchList(filterdList);
+  };
+
+  // console.log(watchList);
   useEffect(() => {
-    axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=23c7a8ad79d43490f5f66984e0bdb17b&page=${pageNo}`)
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/movie/day?api_key=23c7a8ad79d43490f5f66984e0bdb17b&page=${pageNo}`
+      )
       .then(function (res) {
         // console.log(pageNo);
         setMovies(res.data.results);
-      })
-  },[pageNo])
+      });
+  }, [pageNo]);
+
+  useEffect(() => {
+    let moviesFromLocalStorage = localStorage.getItem("IMDB");
+    setWatchList(JSON.parse(moviesFromLocalStorage));
+  }, [pageNo]);
 
   return (
     <div>
       <div className="text-center font-bold text-2xl p-4">Trending Movies</div>
       <div className="flex flex-wrap justify-center">
         {movies.map((movieObj) => {
-          console.log(movieObj);
-          return <MovieCard key={movieObj.id} name={movieObj.title} poster_path={movieObj.poster_path} />
+          return (
+            <MovieCard
+              key={movieObj.id}
+              id={movieObj.id}
+              watchList={watchList}
+              name={movieObj.title}
+              poster_path={movieObj.poster_path}
+              handleAddToWatchList={handleAddToWatchList}
+              handleRemoveFromWatchList={handleRemoveFromWatchList}
+            />
+          );
         })}
       </div>
-      <Pagination pageNo={pageNo} handleClickPrev={handleClickPrev} handleClickNext={handleClickNext}/>
+      <Pagination
+        pageNo={pageNo}
+        handleClickPrev={handleClickPrev}
+        handleClickNext={handleClickNext}
+      />
     </div>
   );
 }
